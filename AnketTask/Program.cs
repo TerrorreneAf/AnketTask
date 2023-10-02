9e9e03bae5050690b9b8d8c9fe140b1ddba937d8
Name <email@example.ru>
2023-10-02 21:08:14 +0300

    Добавьте файлы проекта.

---
+++ AnketTask/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnketTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
